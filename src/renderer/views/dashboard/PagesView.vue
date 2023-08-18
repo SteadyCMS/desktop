e1d3c008f44105262ecef5512c97d5b0bc69f4b9
Noah Rahm <correctsyntax@yahoo.com>
2023-08-18 10:53:48 -0500

    Implement posts list item

--- src/renderer/views/dashboard/PagesView.vue
+++ src/renderer/views/dashboard/PagesView.vue
@@ -1,14 +1,3 @@
 <template>
-  <div class="w-full my-5 mx-8">
-    <div class="flex flex-grow align-center justify-between">
-      <h1 class="text-3xl text-dark font-bold">Pages</h1>
-      <button class="py-2.5 px-6 bg-accent text-dark text-sm font-bold rounded-lg">Add Page</button>
-    </div>
-    <hr class="border border-slate-200 w-full my-4" />
-  </div>
 
-
-
-
-  
 </template>
--- src/renderer/views/dashboard/PostsView.vue
+++ src/renderer/views/dashboard/PostsView.vue
@@ -1,26 +1,24 @@
+<script setup>
+  import AccentButton from '../../components/buttons/AccentButton.vue';
+</script>
+
 <template>
-  <div class="w-full my-5 mx-8">
-    <div class="flex flex-grow align-center justify-between">
-      <h1 class="text-3xl text-dark font-bold">Posts</h1>
-      <button class="py-2.5 px-6 bg-accent text-dark text-sm font-bold rounded-lg">Add Post</button>
+  <div class="w-full my-4 mx-8">
+    <div class="flex flex-grow align-center items-center justify-between mx-6">
+      <h1 class="text-4xl text-dark font-bold">Posts</h1>
+      <AccentButton text="Add Post" />
     </div>
-    <hr class="border border-slate-200 w-full my-4" />
+
+    <div class="flex flex-col mt-12 space-y-2">
+
+      <div class="rounded-lg cursor-pointer py-5 px-6 bg-slate-50 hover:bg-slate-100 duration-500">
+        <div class="flex flex-row justify-between items-center">
+          <h4 class="flex items-center text-xl text-dark font-bold">Sunshine and storm clouds <span class="text-sm text-slate-500 ml-1 font-bold">&mdash; Draft</span></h4>
+          <span class="text-xs text-slate-500">Aug 17, 2023 5:12pm</span>
+        </div>
+        <p class="text-slate-500 text-sm mt-1 max-w-2xl truncate">Borders are a great way to distinguish two elements from one another, but using too many of them can make your design feel busy and cluttered.</p>
+      </div>
+
+    </div>
   </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
 </template>
