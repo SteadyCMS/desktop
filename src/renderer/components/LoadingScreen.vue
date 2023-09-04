c3ae5c8c10d3703eaa0f685a5bb17d5436bf02be
Noah Rahm <correctsyntax@yahoo.com>
2023-09-04 11:22:38 -0500

    Update loading screen styling

--- src/renderer/components/LoadingScreen.vue
+++ src/renderer/components/LoadingScreen.vue
@@ -12,8 +12,8 @@
           <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
           <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
         </svg>
-        <p class="text-center mt-6 text-md text-slate-600 font-semibold">{{ props.title }}</p>
-        <p class="text-center text-md text-slate-600 font-semibold">{{ props.text }}</p>
+        <p class="text-center mt-6 text-tint-10 font-semibold">{{ props.title }}</p>
+        <p class="text-center text-sm mt-1 text-tint-8">{{ props.text }}</p>
       </div>
     </div>
   </Transition>
